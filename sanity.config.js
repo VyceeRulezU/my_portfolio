befc356c1d64b930973e529a9676407dd8bc4e9b
Joseph-Brendan <199966678+Ironali@users.noreply.github.com>
2026-05-09 20:32:13 +0100

    feat: add custom icon to Sanity studio configuration

--- sanity.config.js
+++ sanity.config.js
@@ -2,10 +2,16 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import React from 'react'
+
+const PortfolioIcon = () => (
+  React.createElement('img', { src: '/favicon.png', alt: 'Portfolio Icon', style: { width: '100%', height: '100%', objectFit: 'contain' } })
+)
 
 export default defineConfig({
   name: 'default',
   title: 'Portfolio Studio',
+  icon: PortfolioIcon,
 
   projectId: '9hecsvz8',
   dataset: 'production',
